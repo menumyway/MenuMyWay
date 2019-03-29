03a6aa6f1f6fc751077db6d03000c7d6ef0390b1
Irais1 <iraisgoparosorio@Iraiss-MacBook-Pro.local>
2019-03-29 10:34:06 -0700

    first commit

---
+++ MenuMyWay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuMyWay
+//
+//  Created by Irais Gopar osorio on 3/27/19.
+//  Copyright © 2019 Irais Gopar Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
