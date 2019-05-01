4209eec260ef56de418ebaf4f016bba7b9c63b2a
Celina Juarez <cejuarez@csumb.edu>
2019-04-30 20:39:10 -0700

    Currency exchange

--- MenuMyWay_1/SavedViewController.swift
+++ MenuMyWay_1/SavedViewController.swift
@@ -17,17 +17,32 @@ class SavedViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         return currencyType.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if(pickerView == currencyPicker) {
+            currentType = currencyType[row]
+        }else if(pickerView == currencyPicker2) {
+            currentType2 = currencyType[row]
+        }
         return currencyType[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        currentCurr = currencyValue[row]
+        if(pickerView == currencyPicker) {
+            currentCurr = currencyValue[row]
+        }else if(pickerView == currencyPicker2) {
+            currentCurr2 = currencyValue[row]
+        }
+        
     }
     
 
     var currencyValue:[Double] = []
     var currencyType:[String] = ["EUR","GBP","MXN", "USD"]
     var currentCurr = 0.0
+    var currentCurr2 = 0.0
+    var currentType = ""
+    var currentType2 = ""
     
+    @IBOutlet weak var currencyPicker2: UIPickerView!
     @IBOutlet weak var currentCurrencyField: UITextField!
     @IBOutlet weak var currencyPicker: UIPickerView!
     @IBOutlet weak var resultCurrencyField: UITextField!
@@ -37,7 +52,7 @@ class SavedViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url  = URL(string: "http://data.fixer.io/api/latest?access_key=")
+        let url  = URL(string: "http://data.fixer.io/api/latest?access_key=9935bfc59303dcd6030f9e32a4f5de4e")
         
         let task = URLSession.shared.dataTask(with: url!) { (data,response,error) in
             if error != nil{
@@ -63,14 +78,25 @@ class SavedViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
                 }
             }
             self.currencyPicker.delegate = self
+            self.currencyPicker2.delegate = self
         }
         task.resume()
     }
     
     @IBAction func onConvertBtn(_ sender: Any) {
         if(currentCurrencyField.text != ""){
-            resultCurrencyField.text = String(Double(currentCurrencyField.text!)! * currentCurr)
+            
+            if(currentType == "EUR") {
+                resultCurrencyField.text = String(format: "%.2f",(Double(currentCurrencyField.text!)! * currentCurr2))
+            }else {
+                var eurConvert = 0.0
+                eurConvert = (Double(currentCurrencyField.text!)! / currentCurr)
+                resultCurrencyField.text = String(format: "%.2f",(eurConvert * currentCurr2))
+            }
+            
         }
     }
+    
+    
     
 }
